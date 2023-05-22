9d5170f44c4b1a522301d6c426a6675b1134f327
Krihag <125972171+Krihag@users.noreply.github.com>
2023-05-22 15:57:49 +0200

    Phone
    added Phone.js with code. Also added text to readme

---
+++ js/phone.js
@@ -0,0 +1,49 @@
+const phone = {
+  brand: "iphone",
+  model: 14,
+  color: "black",
+  battery: 20,
+  powered: true,
+  apps: ["Camera", "Outlook", "Messenger", "Github", "Safari"],
+
+  // Charge battery function
+  chargeBattery: function () {
+    console.log("Charging battery...");
+    this.battery += 20;
+    console.log(`Battery is now at ${this.battery}%`);
+  },
+
+  // Make call function
+  makeCall: function (person) {
+    if (this.powered) {
+      console.log(`Calling ${person}`);
+      this.battery -= 10;
+      this.checkBattery();
+    } else {
+      console.log(`Your phone is off, you need to turn it back on to use it`);
+    }
+  },
+
+  //Check battery function
+  checkBattery: function () {
+    console.log(`The battery is at: ${this.battery}%`);
+    if (this.battery <= 0) {
+      this.powered = false;
+      console.log("Your phone has no battery left. It is now off.");
+    } else console.log(`Your phone has ${this.battery}% battery left`);
+  },
+
+  //Turn on phone function
+  powerPhoneOn: function () {
+    if (this.powered === false) {
+      if (this.battery >= 20) {
+        console.log("Phone is now powered back on..");
+        this.powered = true;
+      } else {
+        console.log(
+          `Your battery is only ${this.battery}. You need atleast 20% to power phone back on.`
+        );
+      }
+    } else console.log("Your phone is allready on");
+  },
+};
